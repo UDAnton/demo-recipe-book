9eac8ecf903092395e015c4709f827265c042be2
udanton <uadjob@gmail.com>
2018-12-11 23:24:52 +0200

    Initial Project

---
+++ src/main/java/com/github/udanton/demorecipebook/DemoRecipeBookApplication.java
@@ -0,0 +1,12 @@
+package com.github.udanton.demorecipebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRecipeBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRecipeBookApplication.class, args);
+    }
+}

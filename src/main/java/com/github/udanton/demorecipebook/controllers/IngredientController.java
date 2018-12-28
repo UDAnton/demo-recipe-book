2c8870a7bc0eb721f0aa824f373fdab1c859040d
UDAnton <uadjob@gmail.com>
2018-12-28 11:55:12 +0200

    View ingredients

---
+++ src/main/java/com/github/udanton/demorecipebook/controllers/IngredientController.java
@@ -0,0 +1,30 @@
+package com.github.udanton.demorecipebook.controllers;
+
+import com.github.udanton.demorecipebook.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    @Autowired
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String ingredients(@PathVariable String recipeId, Model model) {
+        log.debug("Getting ingredient list for recipe id: " + recipeId);
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+        return "recipe/ingredient/list";
+    }
+}
--- src/main/java/com/github/udanton/demorecipebook/controllers/RecipeController.java
+++ src/main/java/com/github/udanton/demorecipebook/controllers/RecipeController.java
@@ -2,11 +2,13 @@ package com.github.udanton.demorecipebook.controllers;
 
 import com.github.udanton.demorecipebook.commands.RecipeCommand;
 import com.github.udanton.demorecipebook.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -20,6 +22,7 @@ public class RecipeController {
     @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
+        log.debug("Show recipe info with id: " + id);
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
     }
@@ -41,6 +44,7 @@ public class RecipeController {
     @GetMapping
     @RequestMapping("/recipe/{id}/delete")
     public String deleteRecipe(@PathVariable String id) {
+        log.debug("Delete recipe with id: " + id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
     }
@@ -48,6 +52,7 @@ public class RecipeController {
     @PostMapping
     @RequestMapping("recipe")
     public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        log.debug("Create or update recipe");
         RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
         return "redirect:/recipe/" + savedRecipeCommand.getId() + "/show";
     }

ce3bfcc837afb1d46c76e58715ce0b3730966b31
UDAnton <uadjob@gmail.com>
2018-12-29 12:47:24 +0200

    Uploading images

---
+++ src/main/java/com/github/udanton/demorecipebook/services/ImageService.java
@@ -0,0 +1,7 @@
+package com.github.udanton.demorecipebook.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long recipeId, MultipartFile file);
+}

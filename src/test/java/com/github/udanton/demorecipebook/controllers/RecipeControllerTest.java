da47d925d612dfb55d2598d3edc1cad19dc664ad
udanton <uadjob@gmail.com>
2019-01-04 00:38:22 +0200

    Customizing error messages with massage source

--- src/test/java/com/github/udanton/demorecipebook/controllers/RecipeControllerTest.java
+++ src/test/java/com/github/udanton/demorecipebook/controllers/RecipeControllerTest.java
@@ -107,7 +107,7 @@ public class RecipeControllerTest {
 
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .param("id", ""))
+                .param("id", "").param("cookTime", "3000"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/recipeform"));
     }
